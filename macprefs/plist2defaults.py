da8faab53ab6f19d1aaf4cdf2b6d48d4d4dc5033
Andrew Udvare <audvare@gmail.com>
2020-09-13 15:44:05 -0400

    macprefs/plist2defaults: use f-strings

--- macprefs/plist2defaults.py
+++ macprefs/plist2defaults.py
@@ -10,8 +10,10 @@ from .utils import is_simple, setup_logging_stderr, to_str
 __all__ = ('plist_to_defaults_commands', )
 
 
-async def plist_to_defaults_commands(domain: str,
-                                     root: PlistRoot, debug: bool = False) -> AsyncIterator[str]:
+async def plist_to_defaults_commands(
+        domain: str,
+        root: PlistRoot,
+        debug: bool = False) -> AsyncIterator[str]:
     """Given a PlistRoot, generate a series of `defaults write` commands."""
     if domain in BAD_DOMAINS:
         return
@@ -21,7 +23,7 @@ async def plist_to_defaults_commands(domain: str,
 
     yield f'# {domain}'
 
-    prefix = 'defaults write {}'.format(quote(domain))
+    prefix = f'defaults write {quote(domain)}'
     log = setup_logging_stderr(verbose=debug)
 
     for key, value in sorted(root.items()):
@@ -41,14 +43,11 @@ async def plist_to_defaults_commands(domain: str,
             pass
 
         if isinstance(value, bool):
-            yield '{} {} -bool {}'.format(prefix, quote(key),
-                                          'true' if value else 'false')
+            yield f'{prefix} {quote(key)} -bool {"true" if value else "false"}'
         elif isinstance(value, int):
-            yield '{} {} -int {}'.format(prefix, quote(key),
-                                         quote(str(int(value))))
+            yield f'{prefix} {quote(key)} -int {quote(str(value))}'
         elif isinstance(value, float):
-            yield '{} {} -float {}'.format(prefix, quote(key),
-                                           quote(str(value)))
+            yield f'{prefix} {quote(key)} -float {quote(str(value))}'
         elif isinstance(value, bytes):
             if len(value) > 120:
                 continue
@@ -57,31 +56,29 @@ async def plist_to_defaults_commands(domain: str,
         elif isinstance(value, str):
             if len(value) > 120:
                 continue
-            yield '{} {} -string {}'.format(prefix, quote(key), quote(value))
+            yield f'{prefix} {quote(key)} -string {quote(value)}'
         elif isinstance(value, list) and await is_simple(value):
             first = (quote(str(value[0])) +
                      ' \\\n' if len(value) > 1 else quote(str(value[0])))
             key = quote(key)
             spaces = ' ' * (len(prefix) + 1 + len(key) + 1 + 7)
-            rest = ' \\\n'.join([
-                '{}{}'.format(spaces, quote(await to_str(x)))
-                for x in value[1:]
-            ])
-            value = first + rest
-            yield '{} {} -array {}'.format(prefix, key, ''.join(value))
+            # [] required for this line, otherwise TypeError: can only join an
+            # iterable
+            rest = ' \\\n'.join(
+                [f'{spaces}{quote(await to_str(x))}' for x in value[1:]])
+            yield f'{prefix} {key} -array {"".join(first + rest)}'
         elif isinstance(value, dict) and await is_simple(value):
             dict_values = [
-                '{} {}'.format(quote(await to_str(x)), quote(await to_str(y)))
+                f'{quote(await to_str(x))} {quote(await to_str(y))}'
                 for x, y in value.items()
             ]
-            f_dict_values = (dict_values[0] + ' \\\n'
+            f_dict_values = (f'{dict_values[0]}\\\n'
                              if len(dict_values) > 1 else dict_values[0])
             key = quote(key)
             spaces = ' ' * (len(prefix) + 1 + len(key) + 1 + 10)
-            dict_values_ = ' \\\n'.join('{}{}'.format(spaces, x)
+            dict_values_ = ' \\\n'.join(f'{spaces}{x}'
                                         for x in dict_values[1:])
-            dict_values_ = f_dict_values + dict_values_
-            yield '{} {} -dict {}'.format(prefix, key, dict_values_)
+            yield f'{prefix} {key} -dict {f_dict_values}{dict_values_}'
         elif isinstance(value, datetime):
             full_date = quote(value.strftime('%Y-%m-%d %I:%M:%S +0000'))
             yield f'{prefix} {quote(key)} -date {full_date}'
