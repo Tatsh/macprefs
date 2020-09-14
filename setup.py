4097ab03109fda2e315518ab7867e78bca98b864
Andrew Udvare <audvare@gmail.com>
2020-09-14 03:54:46 -0400

    setup: lint

--- setup.py
+++ setup.py
@@ -5,17 +5,12 @@ setup(
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     description='Command and library to export macOS preferences.',
-    entry_points={
-        'console_scripts': [
-            'prefs-export = macprefs.command:main',
-        ]
-    },
-    install_requires=[],
+    entry_points={'console_scripts': ['prefs-export = macprefs.command:main']},
     license='LICENSE.txt',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     name='macprefs',
     packages=['macprefs'],
     tests_require=['coveralls', 'nose', 'requests-mock'],
     url='https://github.com/Tatsh/macprefs',
-    version='0.0.1',
-)
+    version='0.0.1')
