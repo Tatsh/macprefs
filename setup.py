89bccd826cfd4e9db164dcab7800702e7d284cb6
Andrew Udvare <audvare@gmail.com>
2022-02-04 06:36:23 -0500

    setup: update and fix license

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 """Module for macprefs."""
 from setuptools import find_packages, setup
 
-setup(author='Andrew Udvare',
-      author_email='audvare@gmail.com',
-      description='Command and library to export macOS preferences.',
-      entry_points={
-          'console_scripts': [
+with open('README.md') as f:
+    setup(author='Andrew Udvare',
+          author_email='audvare@gmail.com',
+          description='Command and library to export macOS preferences.',
+          entry_points=dict(console_scripts=[
+              'macprefs-install-job = macprefs.command:install_job',
               'prefs-export = macprefs.command:main',
-              'macprefs-install-job = macprefs.command:install_job'
-          ]
-      },
-      license='LICENSE.txt',
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      name='macprefs',
-      packages=find_packages(),
-      python_requires='>=3.9',
-      tests_require=['coveralls', 'nose', 'requests-mock'],
-      url='https://github.com/Tatsh/macprefs',
-      version='0.2.1')
+          ], ),
+          license='MIT',
+          long_description=f.read(),
+          long_description_content_type='text/markdown',
+          name='macprefs',
+          packages=find_packages(),
+          python_requires='>=3.9',
+          tests_require=['coveralls', 'nose', 'requests-mock'],
+          url='https://github.com/Tatsh/macprefs',
+          version='0.2.1')
