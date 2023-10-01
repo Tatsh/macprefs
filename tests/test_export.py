13229c58b3d9936b6859ca18b7a1d6ffc8ba8b9d
Andrew Udvare <audvare@gmail.com>
2023-09-30 23:48:06 -0400

    More tests

---
+++ tests/test_export.py
@@ -0,0 +1,55 @@
+from asyncio.subprocess import Process
+import base64
+from io import BytesIO
+from pathlib import Path
+from typing import Any
+
+from click.testing import CliRunner
+from mock import MagicMock
+from pytest_mock.plugin import MockerFixture
+
+from macprefs import prefs_export
+from os.path import splitext
+
+# spell-checker: disable
+SAFARI_PLIST_BASE64 = ('YnBsaXN0MDDRAQJfEBFSZXNldENsb3VkSGlzdG9yeQkICx8AAAAAAAABAQAAAAAAAAADAAAAAAA'
+                       'AAAAAAAAAAAAAIA==')
+# spell-checker: enable
+
+
+def create_entry(name: str) -> MagicMock:
+    m = MagicMock(spec=Path)
+    m.name = name
+    m.stem = splitext(name)[0]
+    return m
+
+
+def test_export_no_args_no_git_no_plutil(runner: CliRunner, mocker: MockerFixture) -> None:
+    def sp_shell_mock(command: str, **kwargs: Any) -> MagicMock:
+        p = MagicMock(spec=Process)
+        p.returncode = 1
+        return p
+
+    def path_truediv(append: str) -> MagicMock:
+        m = MagicMock(spec=Path)
+        if append == 'Library/Preferences':
+            m.glob.return_value = (create_entry('$(PRODUCT_BUNDLE_IDENTIFIER).plist'),
+                                   create_entry('.aaaa.plist'),
+                                   create_entry('com.apple.Safari.plist'),
+                                   create_entry('com.apple.Music.plist'),
+                                   create_entry('com.apple.TV.plist'))
+        return m
+
+    path_mock = mocker.patch('macprefs.command.Path')
+    path_mock.home.return_value.__truediv__.side_effect = path_truediv
+    # _setup_out_dir
+    path_mock.return_value.resolve.return_value.__truediv__.return_value = MagicMock(spec=Path)
+    # plist_out in _defaults_export()
+    (path_mock.return_value.resolve.return_value.__truediv__.return_value.__truediv__.return_value.
+     open.return_value.__enter__.return_value) = BytesIO(base64.b64decode(SAFARI_PLIST_BASE64))
+    shell = mocker.patch('macprefs.command.sp.create_subprocess_shell', side_effect=sp_shell_mock)
+    shutil_copy_mock = mocker.patch('macprefs.command.shutil.copy')
+    run = runner.invoke(prefs_export, '--debug')
+    assert shutil_copy_mock.call_count >= 1
+    assert run.exit_code == 1
+    assert shell.call_count == 1
--- tests/test_install_job.py
+++ tests/test_install_job.py
@@ -35,13 +35,14 @@ def test_install_job_no_args(runner: CliRunner, mocker: MockerFixture) -> None:
     path_mock.return_value.resolve.return_value = 'output-dir'
     (path_mock.home.return_value.__truediv__.return_value.open.return_value.__enter__.return_value.
      write.side_effect) = write
-    runner.invoke(install_job, '--debug')
+    run = runner.invoke(install_job, '--debug')
     path_mock.home.return_value.__truediv__.return_value.open.return_value.__enter__.assert_called()
     path_mock.return_value.resolve.assert_called()
     data = plistlib.loads(written)
     assert not DeepDiff(
         data, EXPECTED_PLIST, exclude_paths=['StandardErrorPath', 'StandardOutPath'])
     assert sp_exec.await_count == 4
+    assert run.exit_code == 0
 
 
 def test_install_exec_error(runner: CliRunner, mocker: MockerFixture) -> None:
@@ -52,7 +53,8 @@ def test_install_exec_error(runner: CliRunner, mocker: MockerFixture) -> None:
     sp_exec.return_value.returncode = 1
     path_mock = mocker.patch('macprefs.command.Path')
     path_mock.return_value.resolve.return_value = 'output-dir'
-    runner.invoke(install_job, '--debug')
+    run = runner.invoke(install_job, '--debug')
     path_mock.home.return_value.__truediv__.return_value.open.return_value.__enter__.assert_called()
     path_mock.return_value.resolve.assert_called()
     assert sp_exec.await_count == 4
+    assert run.exit_code != 0
